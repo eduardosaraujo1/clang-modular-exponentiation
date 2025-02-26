f59734f07d286fb14d8f2ea31c6c178357cca20b
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-26 00:40:49 -0300

    refactor: second iteration following manual exponentiation optimization

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
-int modularExponentiation(int exponent, int base, int modulo, int increment)
+int modularExponentiation(int exponent, int base, int modulo)
 {
-    if (increment == exponent)
+    printf("Starting function call with exponent=%d, base=%d, modulo=%d\n", exponent, base, modulo);
+    if (exponent == 1)
     {
-        return fmod(pow(base, increment), modulo);
+        return base % modulo;
     }
 
-    return modularExponentiation(exponent, base, modulo, increment + 1);
+    int expo = modularExponentiation(exponent / 2, base, modulo);
+    int result = (expo * expo) % modulo;
+
+    if (exponent % 2 == 1)
+    {
+        result *= base;
+        result = result % modulo;
+    }
+    printf("Result is %d\n", result);
+
+    return result;
 }
 
 int main()
 {
-    int result = modularExponentiation(200, 30, 100, 1);
+    int result = modularExponentiation(5, 50, 101);
 
-    printf("%d", result);
+    printf("Final Response: %d\n", result);
 }
