34672f8d32ce6e51504abe7a4249dadf1c91bf52
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-03-25 21:20:05 -0300

    project initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    long long base;
+    long long exponent;
+    long long modulus;
+
+    // USER INPUT
+    printf("Base: ");
+    scanf("%lld", &base);
+    printf("Expoente: ");
+    scanf("%lld", &exponent);
+    printf("Módulo: ");
+    scanf("%lld", &modulus);
+    printf("\n");
+
+    // EVALUATE base^exponent % modulus
+    long long accumulator = 1;
+    for (long long i = 0; i < exponent; i++)
+    {
+        accumulator = (accumulator * base) % modulus;
+    }
+    printf("%lld", accumulator);
+
+    return 0;
+}
