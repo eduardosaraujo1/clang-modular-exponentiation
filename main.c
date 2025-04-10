08a8d118d7b8359e063c085a6ea231dc53b44ea8
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-04-10 00:59:39 -0300

    feat: melhorias na validação de entrada

--- main.c
+++ main.c
@@ -1,4 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUFFER_LEN 100
+
+void ler_inteiro_positivo(char prompt[], long long *value)
+{
+    char buffer[BUFFER_LEN];
+    char digits[BUFFER_LEN];
+
+    int success = 0;
+
+    while (success == 0)
+    {
+        // print prompt
+        printf("%s", prompt);
+
+        // read answer from user
+        memset(buffer, 0, sizeof(buffer));
+        if (fgets(buffer, BUFFER_LEN, stdin) == NULL)
+        {
+            printf("Ocorreu um erro ao ler sua entrada. Tente novamente\n");
+            continue;
+        }
+
+        // replace \n with \0
+        for (int i = 0; buffer[i] != '\0'; i++)
+        {
+            if (buffer[i] == '\n')
+            {
+                buffer[i] = '\0';
+                break;
+            }
+        }
+
+        // remove invalid characters
+        int j = 0;
+
+        memset(digits, 0, sizeof(digits));
+        for (int i = 0; buffer[i] != '\0'; i++)
+        {
+            if (isdigit(buffer[i]))
+            {
+                digits[j++] = buffer[i];
+            }
+        }
+
+        // if j is null, probably the string had no numbers
+        if (j <= 0)
+        {
+            printf("O valor digitado deve ser um número inteiro positivo. Por favor tente novamente.\n");
+            continue;
+        }
+
+        // parse digits to long long
+        *value = strtoll(digits, NULL, 10);
+
+        if (*value <= 0)
+        {
+            printf("O valor digitado não pode ser 0. Por favor tente novamente.\n");
+            continue;
+        }
+
+        // end the loop
+        success = 1;
+    }
+}
 
 int main()
 {
@@ -7,23 +75,13 @@ int main()
     long long modulus;
     long long result;
 
-    puts("Aviso: valores depois da vírgula serão ignorados e negativos se tornarão positivos");
-
-    printf("Base: ");
-    scanf("%lld", &base);
-    printf("Expoente: ");
-    scanf("%lld", &exponent);
-    printf("Módulo: ");
-    scanf("%lld", &modulus);
+    ler_inteiro_positivo("Base: ", &base);
+    ler_inteiro_positivo("Expoente: ", &exponent);
+    ler_inteiro_positivo("Módulo: ", &modulus);
     printf("\n");
 
-    // remover negativos
-    base = base >= 0 ? base : -base;
-    exponent = exponent >= 0 ? exponent : -exponent;
-    modulus = modulus >= 0 ? modulus : -modulus;
-
     result = 1;
-    base = base % modulus;
+    base = base % modulus; // otimização
 
     for (long long i = 0; i < exponent; i++)
     {
